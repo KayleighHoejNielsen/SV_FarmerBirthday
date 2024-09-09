356f2b84389ef49603c94c14826c4832654b8513
KayleighHoejNielsen <97343879+KayleighHoejNielsen@users.noreply.github.com>
2024-09-09 11:27:14 +0200

    main branch created

---
+++ FarmerBirthday/FarmerBirthday/BirthdayDialogue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.GameData.Shirts;
+using StardewValley.Menus;
+using StardewValley.Objects;
+using static StardewValley.Menus.CharacterCustomization;
+
+namespace FarmerBirthday
+{
+    public class BirthdayDialogue : Dialogue
+    {
+        public BirthdayDialogue(NPC speaker) : base(speaker, "", "Hapy Birthday const")
+        {
+
+            public void changedDialogue(string str) {
+                string farmer_name = Utility.FilterUserName(this.farmer.Name);
+
+                if (this.speaker != null && this.speaker.Name.Equals("Pierre"))
+                {
+                    str = str.Replace("@", farmer_name);
+
+                    Game1.content.LoadString("happy birthday @!");
+                }
+            }
+        }
+    }
+}
